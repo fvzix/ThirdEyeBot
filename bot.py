ea9c63d8bd0e192b40135697bf6e9d7d5be48bda
fvzix <cardxblood@gmail.com>
2026-06-12 01:36:03 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is working.")
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
